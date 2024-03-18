0da83597d61dac10ba75abda993337f6d2745035
Max McDaniel <maxhmcd08@gmail.com>
2024-03-17 20:26:07 -0600

    first commit

---
+++ public/viewer.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    init();
+};
+
+async function init() {
+    const peer = createPeer();
+    peer.addTransceiver("video", { direction: "recvonly" })
+}
+
+function createPeer() {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.l.google.com:19302"
+            }
+        ]
+    });
+    peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
+
+    return peer;
+}
+
+async function handleNegotiationNeededEvent(peer) {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+    const payload = {
+        sdp: peer.localDescription
+    };
+
+    const { data } = await axios.post('/consumer', payload);
+    const desc = new RTCSessionDescription(data.sdp);
+    peer.setRemoteDescription(desc).catch(e => console.log(e));
+}
+
+async function handleTrackEvent(e) {
+    let stream = await e.streams[0];
+    document.getElementById("videoStream").srcObject = stream;
+};
